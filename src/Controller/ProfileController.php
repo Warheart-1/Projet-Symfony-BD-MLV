a62a7710834baea4633951c506903502c0bda705
Warheart-1 <axel97477@gmail.com>
2022-11-18 12:25:48 +0100

    Page de profile fini

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -3,17 +3,21 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\UserType;
 use App\Repository\UserRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/profile')]
 class ProfileController extends AbstractController
 {
-    public function __construct()
+    public function __construct(protected ManagerRegistry $registry)
     {
     }
+
     #[Route('/', name: 'app_profile')]
     public function index(): Response
     {
@@ -26,10 +30,31 @@ class ProfileController extends AbstractController
     }
 
     #[Route('/edit/{id}', name: 'app_profile_edit')]
-    public function edit(): Response
+    public function edit(Request $request, $id ,UserRepository $userRepository): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_USER');
+
+        if($id != $this->getUser()->getId()){
+            $this->addFlash('danger', 'Vous ne pouvez pas modifier ce profil');
+            return $this->redirectToRoute('app_profile');
+        }
+
+        $userRegister = $this->registry->getRepository(User::class)->find($id);
+
+        $formUser = $this->createForm(UserType::class, $userRegister);
+        $formUser->handleRequest($request);
+
+        if ($formUser->isSubmitted() && $formUser->isValid()) {
+            $this->registry->getManager()->persist($userRegister);
+            $this->registry->getManager()->flush();
+            $this->addFlash('success', 'Profil modifié avec succès');
+            return $this->redirectToRoute('app_profile');
+        }
+
         return $this->render('profile/edit.html.twig', [
             'controller_name' => 'ProfileController',
+            'formUser' => $formUser->createView(),
+            'user' => $userRegister
         ]);
     }
 
